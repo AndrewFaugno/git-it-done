811e30516c20470a9ac9c9ec9e64961dce0e0e87
AndrewFaugno <andrewfaugno825@gmail.com>
2022-05-14 21:28:59 -0400

    added language selector

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,6 +4,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons")
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
@@ -83,8 +85,35 @@ var displayRepos = function(repos, searchTerm) {
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
   }
+}
 
+// get repos that use certain languages
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) { 
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            })
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+}
+
+// if one of the language buttons is pressed send to getfeaturedrepos with that language
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
 }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
